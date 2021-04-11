0ae29ccf1de1ce1cad72778cd1aa2ab124c83443
bharath shanmugam <bharath.shanmugam@gmail.com>
2021-04-11 09:32:46 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "instance_count" {
+  default = 1
+}
+
+variable "key_name" {
+  description = "Private key name to use with instance"
+  default     = "JenkinsTerraform"
+}
+
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "Base AMI to launch the instances"
+
+  # Bitnami NGINX AMI
+  default = "ami-02eac2c0129f6376b"
+}
+
+variable "vpcid" {
+    default = "vpc-073d5d425d86425d4"
+}
+variable "subnetid" {
+    default = "subnet-07ef0ec79444557b8"
+}
+
