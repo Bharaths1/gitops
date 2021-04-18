96681898b52baa1ebd4acff598544b3a251bfd1c
bharath shanmugam <bharath.shanmugam@gmail.com>
2021-04-18 23:27:49 +0530

    elb

--- main.tf
+++ main.tf
@@ -39,6 +39,38 @@ resource "aws_ebs_volume" "firstvol" {
   size              = 10
 }
 
+resource "aws_elb" "myfirstinstanceelb" {
+  name               = "myfirstinstanceelb-terraform"
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  subnets = var.subnetid
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  instances                   = [aws_instance.myfirstinstance[0].id]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "myfirstinstanceelb-terraform"
+  }
+}
+
 # Create Security Group for EC2
 resource "aws_security_group" "default" {
   vpc_id       = var.vpcid
